fb0e55ed306fea61c35a25f816040beec65c4165
nmhossain02 <nmhossain02@gmail.com>
2022-10-04 07:26:07 -0400

    Basic API call is made

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,40 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Clock from './components/Clock'
 
+type timesType = {
+  [index: string]: any,
+  Fajr: string,
+  Duha: string,
+  Dhuhr: string,
+  Asr: string,
+  Maghrib: string,
+  Isha: string
+}
+
+const defaultTimes: timesType = {
+  Fajr: "12:34 am",
+  Duha: "12:34 am",
+  Dhuhr: "12:34 am",
+  Asr: "12:34 am",
+  Maghrib: "12:34 am",
+  Isha: "12:34 am"
+}
+
 function App() {
   const [currentTime, setCurrentTime] = useState<Date>(new Date())
+  const [times, setTimes] = useState<timesType>(defaultTimes)
 
   useEffect(() => {
     setInterval(() => {
       setCurrentTime(new Date())
     }, 500)
+    const fetchTimes = () => {
+      fetch("http://www.islamicfinder.us/index.php/api/prayer_times?country=US&zipcode=30309&method=2")
+        .then(res => res.json())
+        .then(res => setTimes(res.results))
+    }
+    fetchTimes()
+    setInterval(fetchTimes, 1000 * 30)
   }, [])
 
   return (
@@ -26,42 +53,15 @@ function App() {
         </div>
       </div>
       <div className="side-bar">
-        <div className="time-item">
-          <div className="time-label">
-            Label
+        {Object.keys(times).map(time => {
+          const timeArr = times[time].split(/[: %]+/)
+          return <div className="time-item">
+            <div className="time-label">
+              {time}
+            </div>
+            <Clock hours={timeArr[0]} minutes={timeArr[1]} ampm={timeArr[2]}></Clock>
           </div>
-          <Clock noSeconds={true} date={currentTime}></Clock>
-        </div>
-        <div className="time-item">
-          <div className="time-label">
-            Label
-          </div>
-          <Clock noSeconds={true} date={currentTime}></Clock>
-        </div>
-        <div className="time-item">
-          <div className="time-label">
-            Label
-          </div>
-          <Clock noSeconds={true} date={currentTime}></Clock>
-        </div>
-        <div className="time-item">
-          <div className="time-label">
-            Label
-          </div>
-          <Clock noSeconds={true} date={currentTime}></Clock>
-        </div>
-        <div className="time-item">
-          <div className="time-label">
-            Label
-          </div>
-          <Clock noSeconds={true} date={currentTime}></Clock>
-        </div>
-        <div className="time-item">
-          <div className="time-label">
-            Label
-          </div>
-          <Clock noSeconds={true} date={currentTime}></Clock>
-        </div>
+        })}
       </div>
     </div>
   )
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -8,39 +8,39 @@ type clockProps = {
     ampm: "am" | "pm"
 }
 
-const isClockProps = (x: any) : x is clockProps => {
-    return x.hours && x.minutes && x.seconds && (x.ampm === "am"|| x.ampm === "pm")
+const isClockProps = (x: any): x is clockProps => {
+    return x.hours && x.minutes && (x.ampm === "am" || x.ampm === "pm")
 }
 
-const Digit = ({val, clockHeight="var(--clock-height)"} : {val: number, clockHeight?: string}) => {
+const Digit = ({ val, clockHeight = "var(--clock-height)" }: { val: number, clockHeight?: string }) => {
     const style = {
         translate: `0 calc(${clockHeight} * ${-val})`,
         height: clockHeight
     }
     return <div style={style} className="Digit">
-        {[...Array(10).keys()].map(n => <div style={{height: clockHeight}}key={n} className="digit-pane">
+        {[...Array(10).keys()].map(n => <div style={{ height: clockHeight }} key={n} className="digit-pane">
             <div className={n === val ? "highlight" : "lowlight"}>{n}</div>
         </div>)}
     </div>
-} 
+}
 
-const Clock = (props : clockProps | {date: Date, noSeconds?: boolean}) => {
+const Clock = (props: clockProps | { date: Date, noSeconds?: boolean }) => {
     const ref = useRef<any>()
     const clockHeight = useRef<string>("var(--clock-height)")
     useEffect(() => {
         clockHeight.current = getComputedStyle(ref.current).fontSize
     }, [])
-    const {hours, minutes, seconds, ampm} = 
+    const { hours, minutes, seconds, ampm } =
         isClockProps(props) ? props
-        : {
-            hours: (props.date.getHours() - 1) % 12 + 1,
-            minutes: props.date.getMinutes(),
-            seconds: props.date.getSeconds(),
-            ampm: props.date.toLocaleString('en-US', { hour12: true }).slice(-2).toLowerCase()
-        }
+            : {
+                hours: (props.date.getHours() - 1) % 12 + 1,
+                minutes: props.date.getMinutes(),
+                seconds: props.date.getSeconds(),
+                ampm: props.date.toLocaleString('en-US', { hour12: true }).slice(-2).toLowerCase()
+            }
     const hoursArr = [Math.floor(hours / 10), hours % 10]
     const minutesArr = [Math.floor(minutes / 10), minutes % 10]
-    const noSeconds = function() {
+    const noSeconds = function () {
         if (isClockProps(props))
             return props.seconds === undefined
         else {
@@ -51,7 +51,7 @@ const Clock = (props : clockProps | {date: Date, noSeconds?: boolean}) => {
             }
         }
     }()
-    const secondsArr = noSeconds ? null : [Math.floor(seconds / 10), seconds % 10]
+    const secondsArr = noSeconds ? null : [Math.floor((seconds || 0) / 10), (seconds || 0) % 10]
     const style = {
         height: `calc(${clockHeight.current})`
     }
@@ -59,7 +59,7 @@ const Clock = (props : clockProps | {date: Date, noSeconds?: boolean}) => {
         <Digit clockHeight={clockHeight.current} val={hoursArr[0]} /><Digit clockHeight={clockHeight.current} val={hoursArr[1]} />
         <span className="small-font">:</span><Digit clockHeight={clockHeight.current} val={minutesArr[0]} /><Digit clockHeight={clockHeight.current} val={minutesArr[1]} />
         {!noSeconds && <>
-            <span className="small-font">:</span><Digit clockHeight={clockHeight.current} val={secondsArr[0]} /><Digit clockHeight={clockHeight.current} val={secondsArr[1]} />
+            <span className="small-font">:</span><Digit clockHeight={clockHeight.current} val={secondsArr ? secondsArr[0] : 0} /><Digit clockHeight={clockHeight.current} val={secondsArr ? secondsArr[1] : 0} />
         </>}
         <div className="small-font ampm">{ampm}</div>
     </div>
